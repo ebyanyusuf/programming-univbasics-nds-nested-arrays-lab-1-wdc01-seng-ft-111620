f88522569b456687de66d8fbea7d332d2730c931
droll-framework-1384 <ebyan.yusuf@gmail.com>
2020-11-09 18:15:58 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,7 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  produce_storage = [CONVENTIONAL_PRODUCE[ORGANIC_PRODUCE]]
+  produce_storage = [[CONVENTIONAL_PRODUCE[ORGANIC_PRODUCE]]]
   
   produce_storage
 end
